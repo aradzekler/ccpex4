c13db32d75280a8d1cfd0bd16fb85dbb5f73734e
Arad Zekler <25280958+aradzekler@users.noreply.github.com>
2018-05-08 20:10:15 +0300

    Update Value.hpp

--- Value.hpp
+++ Value.hpp
@@ -8,9 +8,9 @@ using namespace std;
 class Value{
     public:
         char val;
-        Value(char);
+        Value(char c);
         Value();
-        friend ostream& operator<<(ostream& os, Value const& obj);
+        friend ostream& operator<<(ostream& out, Value const& obj);
         Value& operator=(char c);
         Value& operator=(const Value& c);
         friend bool operator==(const Value c1 , const char c2);
