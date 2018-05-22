a6b6258e39e16d05d214ce1391d3020e955ca03f
Arad Zekler <25280958+aradzekler@users.noreply.github.com>
2018-05-22 15:54:18 +0300

    Add files via upload

---
+++ TicTacToe.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include "Board.h"
+#include "Player.h"
+
+class TicTacToe
+{
+  public:
+    int size;
+    Board* gboard;
+    Player* gwinner;
+
+    TicTacToe(int);
+    ~TicTacToe();
+    Board board() const;
+    Player& winner() const;
+    void play(Player&, Player&);
+    bool check(Coordinates, Board &);
+};
