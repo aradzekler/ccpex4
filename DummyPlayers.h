28ae84ff405cdc1cf045635d87a0f67027af0f0b
Arad Zekler <25280958+aradzekler@users.noreply.github.com>
2018-05-23 18:35:48 +0300

    Update DummyPlayers.h

--- DummyPlayers.h
+++ DummyPlayers.h
@@ -1,52 +1,55 @@
 #pragma once
+#include "Coordinate.hpp"
 #include "Player.h"
+#include "Board.h"
 
-/* This player scans the board looping on x then on y, 
-	and plays on the first empty cell. */
 
+
+/* 
+	This player scans the board looping on x then on y, 
+	and plays on the first empty cell. 
+*/
 class XYPlayer: public Player {
+public:
+	const string name() const override { return "XYPlayer"; }
 
-	public:
-	
-		const string name() const override { return "XYPlayer"; }
-	
-		/*  Selects a single place to put my symbol, given the current board.
-			Defines the strategy of the player. */
-	
-		const Coordinates play(const Board& board) override;
+	/**
+	 * Selects a single place to put my symbol, given the current board.
+	 * Defines the strategy of the player.
+	 */
+	const Coordinate play(const Board& board) override;
 };
 
-/* This player scans the board looping on y then on x, 
-	and plays on the first empty cell. */
 
+/* 
+	This player scans the board looping on y then on x, 
+	and plays on the first empty cell. 
+*/
 class YXPlayer: public Player {
-
-	public:
-
-		const string name() const override { return "YXPlayer"; }
-		const Coordinates play(const Board& board) override;
+public:
+	const string name() const override { return "YXPlayer"; }
+	const Coordinate play(const Board& board) override;
 };
 
-/*  This player makes illegal moves - 
-	it tries to override cells of the other player.
-	It should always lose. */
 
+/* 
+   This player makes illegal moves - 
+   it tries to override cells of the other player.
+   It should always lose.
+*/
 class IllegalPlayer: public Player {
-
-	public:
-	
-		const string name() const override { return "YXPlayer"; }
-		const Coordinates play(const Board& board) override;
+public:
+	const string name() const override { return "YXPlayer"; }
+	const Coordinate play(const Board& board) override;
 };
 
 
-/* This player always throws an exception.
-	It should always lose. */
-
+/* 
+	This player always throws an exception.
+	It should always lose. 
+*/
 class ExceptionPlayer: public Player {
-
-	public:
-	
-		const string name() const override { return "YXPlayer"; }
-		const Coordinates play(const Board& board) override;
+public:
+	const string name() const override { return "YXPlayer"; }
+	const Coordinate play(const Board& board) override;
 };
