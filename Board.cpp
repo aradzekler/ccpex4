0a3ac663a85c8ae0ed5483ab851162c722370c13
Arad Zekler <25280958+aradzekler@users.noreply.github.com>
2018-06-03 23:46:04 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -188,7 +188,7 @@ string Board::draw(int size)
     {
         for (int j = 0; j < size; ++j)
         {
-            imagef.write(reinterpret_cast<char *>(&image[i][j]), 2);
+            imagef.write(reinterpret_cast<char *>(&image[i][j]), 3);
         }
     }
     imagef.close();
