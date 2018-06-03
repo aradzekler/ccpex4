37acd1d961103b2615177dcbc138f5fefbe4f9b2
Arad Zekler <25280958+aradzekler@users.noreply.github.com>
2018-06-03 18:42:27 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -107,3 +107,190 @@ ostream &operator<<(ostream &out, Board const &obj)
     }
     return out;
 }
+
+istream &operator>>(istream &in, Board &obj)
+{
+    string line;
+    cin >> line;
+    obj.size = line.size();
+    obj.board = new Value *[obj.size];
+    for (int i = 0; i < obj.size; i++)
+    {
+        obj.board[i] = new Value[obj.size];
+        for (int j = 0; j < obj.size; j++)
+        {
+            obj.board[i][j] = line[j];
+        }
+        if (i != obj.size - 1)
+            cin >> line;
+    }
+    return in;
+}
+
+string Board::draw(int size)
+{
+    int numfile = 0;
+    while (true)
+    {
+        ifstream f(to_string(numfile) + ".ppm");
+        if (f.bad()) // if stream is inturupted
+            break;
+        else
+            numfile++;
+    }
+    fname = to_string(numfile) + ".ppm";
+    const int sizeOfArr = size;
+    ofstream imagef(fname, ios::out | ios::binary);
+
+    imagef << "P6" << endl
+           << size << " " << size << endl
+           << 255 << endl;
+
+    Color **image = new Color *[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        image[i] = new Color[size];
+        for (int j = 0; j < size; j++)
+        {
+            image[i][j].red = 255;
+            image[i][j].green = 255;
+            image[i][j].blue = 255;
+        }
+    }
+
+    drawboard(image, size);
+    for (int k = 0; k < bsize; k++) // draw signs accordingly
+    {
+        for (int j = 0; j < bsize; j++)
+        {
+            if (board[k][j].val == 'X')
+            {
+                drawX(image, size, k, j);
+            }
+            else if (board[k][j].val == 'O')
+            {
+                drawO(image, size, k, j);
+            }
+        }
+    }
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; ++j)
+        {
+            imagef.write(reinterpret_cast<char *>(&image[i][j]), 3);
+        }
+    }
+    imagef.close();
+    for (int i = 0; i < size; i++) // cleaning up
+        delete[] image[i];
+    delete[] image;
+
+    return fname;
+}
+
+void Board::drawboard(Color **image, int size)
+{
+    int cell = (size - 20 * (bsize - 1)) / bsize;
+    for (int i = 0; i < size; ++i)
+    {
+        for (int k = 1; k <= bsize; k++)
+        {
+            for (int j = (cell * k) + 20 * (k - 1); j < (cell * k) + 20 * k && j < size; ++j)
+            {
+                image[j][i].red = 0;
+                image[j][i].green = 0;
+                image[j][i].blue = 0;
+                image[i][j].red = 0;
+                image[i][j].green = 0;
+                image[i][j].blue = 0;
+            }
+        }
+    }
+}
+
+void Board::drawX(Color **image, int size, int k, int j)
+{
+    int cell = (size - 20 * (bsize - 1)) / bsize;
+    for (int i = 0; i < cell; ++i)
+    {
+        image[(cell + 20) * k + i][(cell + 20) * j + i].red = 255;
+        image[(cell + 20) * k + i][(cell + 20) * j + i].green = 0;
+        image[(cell + 20) * k + i][(cell + 20) * j + i].blue = 0;
+
+        image[(cell + 20) * k + cell - 1 - i][(cell + 20) * j + i].red = 255;
+        image[(cell + 20) * k + cell - 1 - i][(cell + 20) * j + i].green = 0;
+        image[(cell + 20) * k + cell - 1 - i][(cell + 20) * j + i].blue = 0;
+    }
+}
+
+void Board::drawO(Color **image, int size, int k, int j)
+{
+    int cell = (size - 20 * (bsize - 1)) / bsize;
+    int r = cell / 2;
+    int xCent = (cell + 20) * k + r;
+    int yCent = (cell + 20) * j + r;
+    int x = r, y = 0;
+    int P = 1 - r;
+
+    image[x + xCent][y + yCent].red = 255;
+    image[x + xCent][y + yCent].green = 0;
+    image[x + xCent][y + yCent].blue = 0;
+
+    if (r > 0)
+    {
+        image[x + xCent][-y + yCent].red = 255;
+        image[x + xCent][-y + yCent].green = 0;
+        image[x + xCent][-y + yCent].blue = 0;
+        image[y + xCent][x + yCent].red = 255;
+        image[y + xCent][x + yCent].green = 0;
+        image[y + xCent][x + yCent].blue = 0;
+        image[-y + xCent][x + yCent].red = 255;
+        image[-y + xCent][x + yCent].green = 0;
+        image[-y + xCent][x + yCent].blue = 0;
+    }
+    while (x > y)
+    {
+        y++;
+        if (P <= 0)
+        {
+            P = P + 2 * y + 1;
+        }
+        else
+        {
+            x--;
+            P = P + 2 * y - 2 * x + 1;
+        }
+        if (x < y)
+        {
+            break;
+        }
+        image[x + xCent][y + yCent].red = 255;
+        image[x + xCent][y + yCent].green = 0;
+        image[x + xCent][y + yCent].blue = 0;
+        image[-x + xCent][y + yCent].red = 255;
+        image[-x + xCent][y + yCent].green = 0;
+        image[-x + xCent][y + yCent].blue = 0;
+        image[x + xCent][-y + yCent].red = 255;
+        image[x + xCent][-y + yCent].green = 0;
+        image[x + xCent][-y + yCent].blue = 0;
+        image[-x + xCent][-y + yCent].red = 255;
+        image[-x + xCent][-y + yCent].green = 0;
+        image[-x + xCent][-y + yCent].blue = 0;
+        if (x != y)
+        {
+            image[y + xCent][x + yCent].red = 255;
+            image[y + xCent][x + yCent].green = 0;
+            image[y + xCent][x + yCent].blue = 0;
+            image[-y + xCent][x + yCent].red = 255;
+            image[-y + xCent][x + yCent].green = 0;
+            image[-y + xCent][x + yCent].blue = 0;
+            image[y + xCent][-x + yCent].red = 255;
+            image[y + xCent][-x + yCent].green = 0;
+            image[y + xCent][-x + yCent].blue = 0;
+            image[-y + xCent][-x + yCent].red = 255;
+            image[-y + xCent][-x + yCent].green = 0;
+            image[-y + xCent][-x + yCent].blue = 0;
+        }
+    }
+}
